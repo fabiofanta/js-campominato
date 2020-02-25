3bef3e60e7154d9b68e19d5e5658829a8e486946
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-25 18:10:48 +0100

    5-rel

--- js/main.js
+++ js/main.js
@@ -4,16 +4,33 @@
 3- se il numero è incluso nella lista dei numeri generati, si conclude la partita, altrimenti si continua con i prompt successivi
 4-la partita termina se viene inserito un numero di quelli casuali generati dal computer o se si raggiunge il massimo possibile di numeri consentiti.
 5-al termine, il software deve comunicare il numero di volte che l'utente ha inserito un numero consentito
+6-Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50
 */
+
+var difficolta = parseInt((prompt("Scegli un livello da 0 a 2")))
+switch (difficolta) {
+    case 0:
+    var maxNumber = 100;
+    var minNumber = 1;
+    break;
+    case 1:
+    var maxNumber = 80;
+    var minNumber = 1;
+    break;
+    case 2:
+    var maxNumber = 50;
+    var minNumber = 1;
+}
+
 var generateNumbers = [];
 for (var i = 0; i < 16; i++) {
-    var generator = generaRandom(1,100);
+    var generator = generaRandom(minNumber,maxNumber);
     console.log(generator);
     generateNumbers.push(generator);
 }
 
 for (var i = 0; i < 16; i++) {
-    var userPrompt = parseInt(prompt("Inserisci un numero da 1 a 100"));
+    var userPrompt = parseInt(prompt("Inserisci un numero da " + minNumber + " a " + maxNumber));
     if (generateNumbers.includes(userPrompt)) {
         console.log("hai perso");
         console.log("hai accumulato " + i + " punti");
